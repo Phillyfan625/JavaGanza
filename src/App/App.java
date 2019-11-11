3919c27a7b92f72992c23ced213d8ce248a275ce
PJ Johnson <39510645+Phillyfan625@users.noreply.github.com>
2019-11-11 16:23:29 -0500

    Add files via upload
    This is our sprint two changes

---
+++ src/App/App.java
@@ -0,0 +1,23 @@
+package App;
+
+import View.LessonTemplate;
+import View.MainMenu;
+import java.util.ArrayList;
+
+
+public class App
+{
+        public static void main(String[] args){
+            LessonTemplate lessonOne = new LessonTemplate();
+            LessonTemplate lessonTwo = new LessonTemplate();
+            LessonTemplate lessonThree = new LessonTemplate();
+            ArrayList<LessonTemplate> lessons = new ArrayList<>();
+            lessons.add(lessonOne);
+            lessons.add(lessonTwo);
+            lessons.add(lessonThree);
+
+            MainMenu mm = new MainMenu();
+            mm.setVisible(true);
+        }
+
+}
---
+++ src/Model/LessonLogic.java
@@ -0,0 +1,39 @@
+package Model;
+
+
+
+public class LessonLogic
+{
+	// Instance Variables -- define your private data
+        String questions;
+        String answers;
+        
+    
+    
+
+
+	// Constructors
+	public LessonLogic()
+	{
+		// initialize default values
+	}
+	public LessonLogic (int data) // pass in data to initialize variables
+	{
+	}
+
+	// Set methods - one set method for each instance variable defined above
+	//             - purpose is to pass in a value stored in the private variable
+
+	// Get methods - one get method for each instance variable defined above
+	//             - purpose is to return the value stored in the private variable
+
+	// Additional methods -- such as for calculation, display
+
+	public String toString()
+	{
+		// return data as a String
+		return "";
+	}
+
+
+}
